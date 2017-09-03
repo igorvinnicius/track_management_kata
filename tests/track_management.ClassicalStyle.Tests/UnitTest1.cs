1fbb2832a37b4d5334e353d9f725574f08039b40
igorvinnicius <igorvinnicius@gmail.com>
2017-09-03 17:48:59 -0300

    ini tests Classical Style project

---
+++ tests/track_management.ClassicalStyle.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace track_management.ClassicalStyle.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
