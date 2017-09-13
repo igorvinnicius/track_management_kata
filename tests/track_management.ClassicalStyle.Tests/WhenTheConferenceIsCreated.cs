19781d9e4c489d7187e32bf00eccf64ad4218349
vinniciusi <vinniciusi@ryanair.com>
2017-09-13 16:59:34 -0300

    first red test

---
+++ tests/track_management.ClassicalStyle.Tests/WhenTheConferenceIsCreated.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace track_management.ClassicalStyle.Tests
+{
+    public class WhenTheConferenceIsCreated
+    {
+	    [Fact]
+	    public void TracksCountShouldBeZero()
+	    {
+		    var conference = new Conference();
+
+			Assert.Equal(0, conference.Tracks.Count);
+	    }
+
+    }
+}
