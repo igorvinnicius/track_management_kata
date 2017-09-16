46abc3abe5773854a27fe38094612a2bde334a2c
igorvinnicius <igorvinnicius@gmail.com>
2017-09-16 10:13:39 -0300

    afternoon session should finish at a set time

--- tests/track_management.ClassicalStyle.Tests/WellKnownTypes/WellKnownSessions.cs
+++ tests/track_management.ClassicalStyle.Tests/WellKnownTypes/WellKnownSessions.cs
@@ -10,13 +10,26 @@ namespace track_management.ClassicalStyle.Tests.WellKnownTypes
 	    {
 			return new MorningSession();
 	    }
-    }
+
+	    public static AfternoonSession AfternoonSession()
+	    {
+		    return new AfternoonSession();
+	    }
+
+
+	}
 
 
 	public class MorningSession : WellKnownSession
 	{
 		public override TimeSpan StartAt => new TimeSpan(9,0,0);
 		public override TimeSpan FinishAt => new TimeSpan(12, 0, 0);
+	}
+
+	public class AfternoonSession : WellKnownSession
+	{
+		public override TimeSpan StartAt => new TimeSpan(13, 0, 0);
+		public override TimeSpan FinishAt => new TimeSpan(17, 0, 0);
 	}
 
 
--- tests/track_management.ClassicalStyle.Tests/WhenAfternoonSessionIsCreated.cs
+++ tests/track_management.ClassicalStyle.Tests/WhenAfternoonSessionIsCreated.cs
@@ -1,10 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using track_management.ClassicalStyle.Tests.Builders;
+using track_management.ClassicalStyle.Tests.WellKnownTypes;
+using Xunit;
 
 namespace track_management.ClassicalStyle.Tests
 {
-    class WhenAfternoonSessionIsCreated
+    public class WhenAfternoonSessionIsCreated
     {
-    }
+
+	    [Fact]
+	    public void ShouldStartAtASetTime()
+	    {
+		    var sessionBuilder = new SessionBuilder();
+		    var afternoonSession = sessionBuilder.ForWellKnownSession(WellKnownSessions.AfternoonSession()).Build();
+
+		    var expectedStart = new TimeSpan(13, 0, 0);
+
+		    Assert.Equal(expectedStart, afternoonSession.StartAt);
+	    }
+
+	    [Fact]
+	    public void ShouldFinishAtASetTime()
+	    {
+
+		    var sessionBuilder = new SessionBuilder();
+		    var afternoonSession = sessionBuilder.ForWellKnownSession(WellKnownSessions.AfternoonSession()).Build();
+
+		    var expectedFinish = new TimeSpan(17, 0, 0);
+
+		    Assert.Equal(expectedFinish, afternoonSession.FinishAt);
+
+	    }
+	}
 }
