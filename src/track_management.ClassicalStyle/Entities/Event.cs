27a0a32c293d805073da9560d35205130349b087
igorvinnicius <igorvinnicius@gmail.com>
2017-09-17 12:22:53 -0300

    when an event is created

---
+++ src/track_management.ClassicalStyle/Entities/Event.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace track_management.ClassicalStyle.Entities
+{
+    public class Event
+    {
+	    public TimeSpan StartAt { get; private set; }
+
+	    public Event(TimeSpan startTime)
+	    {
+			SetStartTime(startTime);
+	    }
+
+	    public void SetStartTime(TimeSpan startTime)
+	    {
+		    StartAt = startTime;
+	    }
+    }
+}
---
+++ tests/track_management.ClassicalStyle.Tests/WhenAnEventIsCreated.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using track_management.ClassicalStyle.Entities;
+using Xunit;
+
+
+namespace track_management.ClassicalStyle.Tests
+{
+    public class WhenAnEventIsCreated
+    {
+
+	    [Fact]
+	    public void ShouldStartAtASetTime()
+	    {
+		    var expectedStart = new TimeSpan(13, 0, 0);
+
+			var someEvent = new Event(expectedStart);
+
+		    Assert.Equal(expectedStart, someEvent.StartAt);
+		}
+
+
+    }
+}
