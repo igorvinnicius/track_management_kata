295b0ca0422222ce391b66788b5d8337ea0881cb
igorvinnicius <igorvinnicius@gmail.com>
2017-09-17 12:40:17 -0300

    added the EventBuilder

---
+++ tests/track_management.ClassicalStyle.Tests/Builders/EventBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using track_management.ClassicalStyle.Entities;
+
+namespace track_management.ClassicalStyle.Tests.Builders
+{
+    public class EventBuilder
+    {
+	    public TimeSpan DefaultStartAt { get; private set; }
+	    public TimeSpan DefaultFinishAt { get; private set; }
+
+	    public EventBuilder WithStartAt(TimeSpan startAt)
+	    {
+		    this.DefaultStartAt = startAt;
+		    return this;
+	    }
+
+	    public EventBuilder WithFinishAt(TimeSpan finishAt)
+	    {
+		    this.DefaultFinishAt = finishAt;
+		    return this;
+	    }
+
+	   
+
+
+	    public Event Build()
+	    {
+		    var session = new Event(DefaultStartAt, DefaultFinishAt);
+
+		    return session;
+	    }
+
+	}
+}
---
+++ tests/track_management.ClassicalStyle.Tests/WellKnownTypes/WellKnownEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace track_management.ClassicalStyle.Tests.WellKnownTypes
+{
+    public abstract class WellKnownEvent
+    {
+	    public abstract TimeSpan StartAt { get; }
+	    public abstract TimeSpan FinishAt { get; }
+	}
+}
