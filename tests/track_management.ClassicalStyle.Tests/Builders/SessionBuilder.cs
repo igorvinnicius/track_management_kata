8588aba11dce8cebdba46da324498981dc6b938f
igorvinnicius <igorvinnicius@gmail.com>
2017-09-18 17:59:48 -0300

    refactoring SessionBuilder

--- tests/track_management.ClassicalStyle.Tests/Builders/SessionBuilder.cs
+++ tests/track_management.ClassicalStyle.Tests/Builders/SessionBuilder.cs
@@ -12,15 +12,24 @@ namespace track_management.ClassicalStyle.Tests.Builders
 	    public TimeSpan DefaultStartAt { get; private set; }
 	    public TimeSpan DefaultFinishAt { get; private set; }
 
+	    private TimeSpan _startAt;
+	    private TimeSpan _finishAt;
+
+	    public SessionBuilder()
+	    {
+		    WithStartAt(DefaultStartAt);
+		    WithFinishAt(DefaultFinishAt);
+	    }
+
 		public SessionBuilder WithStartAt(TimeSpan startAt)
 		{
-			this.DefaultStartAt = startAt;
+			this._startAt = startAt;
 			return this;
 		}
 
 	    public SessionBuilder WithFinishAt(TimeSpan finishAt)
 	    {
-		    this.DefaultFinishAt = finishAt;
+		    this._finishAt = finishAt;
 		    return this;
 	    }
 
@@ -35,7 +44,7 @@ namespace track_management.ClassicalStyle.Tests.Builders
 
 	    public Session Build()
 	    {
-			var session = new Session(DefaultStartAt, DefaultFinishAt);
+			var session = new Session(_startAt, _finishAt);
 
 		    return session;
 	    }
