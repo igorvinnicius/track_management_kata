023035acd62d1b926cb556e5a1851bfe7d5ec894
igorvinnicius <igorvinnicius@gmail.com>
2017-09-20 16:57:41 -0300

    added ConferenceBuilder and TrackBuilder

---
+++ tests/track_management.ClassicalStyle.Tests/Builders/TrackBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using track_management.ClassicalStyle.Entities;
+
+namespace track_management.ClassicalStyle.Tests.Builders
+{
+    public class TrackBuilder
+    {
+	    public TrackBuilder()
+	    {
+				
+	    }
+
+	    public Track Build()
+	    {
+			var track = new Track();
+
+		    return track;
+
+	    }
+
+    }
+}
