7e364386160c2df4c84896b867424d89cc68eac1
igorvinnicius <igorvinnicius@gmail.com>
2017-09-20 17:14:37 -0300

    added TalkBuilder

---
+++ tests/track_management.ClassicalStyle.Tests/Builders/TalkBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using track_management.ClassicalStyle.Entities;
+
+namespace track_management.ClassicalStyle.Tests.Builders
+{
+    public class TalkBuilder
+    {
+
+	    public TalkBuilder()
+	    {
+				
+	    }
+
+	    public Talk Build()
+	    {
+			var talk = new Talk();
+
+		    return talk;
+	    }
+
+    }
+}
